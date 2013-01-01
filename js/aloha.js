e2d6a435dd1b32eb50ada5320ff07b9df2790d9a
Jesse Reiss <jessereiss@gmail.com>
2012-12-31 16:36:57 -0800

    Wired up

--- js/aloha.js
+++ js/aloha.js
@@ -6,9 +6,9 @@ var interval = setInterval(function() {
     url: url,
     success: function(response) {
       var username, imageUrl, likeCount, caption, timestamp, data, img;
-      data = response["data"] && response["data"][0];
+      data = response["data"] && response["data"][response["data"].length - 1];
       if (data && data["user"] && data["images"] && data["created_time"]) {
-        timestamp = new Date(data["created_time"]);
+        timestamp = new Date(parseInt(data["created_time"], 10) * 1000);
         username = data["user"]["username"];
         imageUrl = data["images"]["standard_resolution"]["url"];
         // Like count
@@ -17,7 +17,7 @@ var interval = setInterval(function() {
           likeCount = data["likes"]["count"];
         }
         // Caption
-        data["caption"]  = "#aloha2013"
+        caption = "#aloha2013";
         if (data["caption"]) {
           caption = data["caption"]["text"];
         }
@@ -25,12 +25,21 @@ var interval = setInterval(function() {
         console.log("Missing data in : ", data, data["user"], data["images"], data["created_time"]);
       }
       img = $("<img>");
-      console.log("Image is now : ", imageUrl);
+      console.log("Image is now : ", imageUrl, " from ", username, " created ", timestamp, " caption ", caption);
       img.attr('src', imageUrl);
       img.attr('alt', caption);
       img.attr('height', 612);
       img.attr('width', 612);
       $('#photo-container').html(img);
+      $('#photo-caption').html(caption);
+      $('#photo-user').html(username);
+      $('#photo-timestamp').html(timestamp);
+      if (likeCount > 0) {
+        $('#photo-likes').html(likeCount);
+        $('#photo-likes').show();
+      } else {
+        $('#photo-likes').hide();
+      }
     }
   })
 }, 1000);
